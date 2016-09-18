73b2de6482418a006e32369000c3173dad47ade0
Dan Watkins <dwatkins@valpineware.com>
2016-09-17 19:08:15 -0500

    Automatically wire ViewModel to View by convention.

---
+++ MvvmDemo/ViewModelLocator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MvvmDemo
+{
+    public class ViewModelLocator
+    {
+        public static readonly DependencyProperty AutoHookedUpViewModelProperty =
+            DependencyProperty.RegisterAttached(
+                "AutoHookedUpViewModel",
+                typeof(bool),
+                typeof(ViewModelLocator),
+                new PropertyMetadata(false, AutoHookedUpViewModelChanged));
+
+        public static bool GetAutoHookedUpViewModel(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(AutoHookedUpViewModelProperty);
+        }
+
+        public static void SetAutoHookedUpViewModel(DependencyObject obj, bool value)
+        {
+            obj.SetValue(AutoHookedUpViewModelProperty, value);
+        }
+
+        private static void AutoHookedUpViewModelChanged(DependencyObject obj, DependencyPropertyChangedEventArgs e)
+        {
+            if (DesignerProperties.GetIsInDesignMode(obj))
+                return;
+
+            var viewType = obj.GetType();
+            string viewTypeName = viewType.FullName.Replace(".View.", ".ViewModel.");
+            var viewModelType = Type.GetType(viewTypeName + "Model");
+            ((FrameworkElement)obj).DataContext = Activator.CreateInstance(viewModelType);
+        }
+    }
+}
